0cfcee49b5e29d9f2f4f77ed3e78bc2815682a08
Dharma030 <borntowindharma@gmail.com>
2023-11-26 19:35:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["FULL STACK DEVELOPER . . " , "Programmer . . ." , "Web Developer . . ."],
+    typeSpeed: 100,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true
+})
